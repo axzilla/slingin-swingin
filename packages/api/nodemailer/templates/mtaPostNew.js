2b85945ffc1be742f5297cbf777449da74254c7b
Axel <axel.adrian@me.com>
2019-05-30 17:30:24 +0200

    Change .env workflow and change server side start scripts

--- packages/api/nodemailer/templates/mtaPostNew.js
+++ packages/api/nodemailer/templates/mtaPostNew.js
@@ -1,16 +1,19 @@
 const transporter = require('../transporter')
-const keys = require('../../config/keys')
 
-module.exports = mtaPostNew = (post) => {
+module.exports = mtaPostNew = post => {
   const mailOptions = {
-    from: keys.nodemailerUser,
+    from: process.env.NODEMAILER_USER,
     to: 'office@codehustla.io',
     subject: 'Neuer Beitrag!',
     html: `
       <p>Hi Admin,</p>
       <p>Es gibt einen neuen Beitrag.</p>
-      <a href="${process.env.ENV_API}/post/${post.shortId}/${post.urlSlug}"><button>Beitrag anschauen</button></a>
-      <p><a href="${process.env.ENV_API}/post/${post.shortId}/${post.urlSlug}">${process.env.ENV_API}/post/${post.shortId}/${post.urlSlug}</a></p>
+      <a href="${process.env.ENV_API}/post/${post.shortId}/${
+      post.urlSlug
+    }"><button>Beitrag anschauen</button></a>
+      <p><a href="${process.env.ENV_API}/post/${post.shortId}/${
+      post.urlSlug
+    }">${process.env.ENV_API}/post/${post.shortId}/${post.urlSlug}</a></p>
       `
   }
 
