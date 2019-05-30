2529fdc6b785c2c346b9dc7312b55a0d9e421352
Axel <axel.adrian@me.com>
2019-05-30 21:26:55 +0200

    Require path on server

--- packages/api/server.js
+++ packages/api/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
