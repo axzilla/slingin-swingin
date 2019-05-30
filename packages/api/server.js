e29d5ce42ababebc9e88457842eaf28294e9f4fb
Axel <axel.adrian@me.com>
2019-05-30 18:26:39 +0200

    Remove console.log from server.js

--- packages/api/server.js
+++ packages/api/server.js
@@ -34,8 +34,6 @@ mongoose
 
 app.use('/public', express.static(__dirname + '../app/public'))
 
-console.log(process.env.NODE_ENV)
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../app/build'))
 
