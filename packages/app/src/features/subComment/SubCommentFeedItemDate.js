5e885286e0448f112333f02b6c9ffaf8d1020cd9
Axel <axel.adrian@me.com>
2019-06-03 19:05:22 +0200

    Remove unused imports

--- packages/app/src/features/subComment/SubCommentFeedItemDate.js
+++ packages/app/src/features/subComment/SubCommentFeedItemDate.js
@@ -3,9 +3,6 @@ import React from 'react'
 import Moment from 'react-moment'
 import 'moment/locale/de'
 
-// Components
-import Link from '../../components/Link'
-
 // Material Core
 import { Typography } from '@material-ui/core'
 
