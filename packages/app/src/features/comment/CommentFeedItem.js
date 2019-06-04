d581a8e5ee619309c6365b4682456d88e1262638
Axel <axel.adrian@me.com>
2019-06-04 10:01:40 +0200

    Start new styling for comment and subcomment components

--- packages/app/src/features/comment/CommentFeedItem.js
+++ packages/app/src/features/comment/CommentFeedItem.js
@@ -1,5 +1,7 @@
 // Packages
 import React, { useState, useEffect } from 'react'
+import Moment from 'react-moment'
+import 'moment/locale/de'
 
 // Contexts
 import { useAuth } from '../../contexts/auth'
@@ -9,6 +11,7 @@ import { updateComment, deleteComment } from './_services'
 import { getSubCommentByCommentId } from '../subComment/_services'
 
 // Component
+import Link from '../../components/Link'
 import CommentEditContainer from './container/CommentEditContainer'
 import CommentFeedItemAvatar from './CommentFeedItemAvatar'
 import CommentFeedItemCreator from './CommentFeedItemCreator'
@@ -26,20 +29,25 @@ import { makeStyles } from '@material-ui/styles'
 import {
   Grid,
   Card,
+  CardHeader,
   CardContent,
+  CardActions,
   Divider,
   List,
-  Button,
-  Typography
+  Avatar,
+  IconButton
 } from '@material-ui/core'
+
+// Material Icons
+import { MoreVert as MoreVertIcon } from '@material-ui/icons'
 
 const useStyles = makeStyles(theme => ({
   card: { marginBottom: '20px' },
   header: {
-    marginBottom: '20px'
+    // marginBottom: '20px'
   },
   buttonContainer: {
-    margin: '-20px auto'
+    // margin: '-20px auto'
   },
   button: {
     margin: theme.spacing(1),
@@ -124,63 +132,71 @@ const CommentFeedItem = ({
   return (
     <Card className={classes.card}>
       {!isEditMode ? (
-        <CardContent>
+        <React.Fragment>
+          <CardHeader
+            avatar={
+              comment.user.avatar ? (
+                <Link to={`/${comment.user.username}`}>
+                  <Avatar
+                    src={
+                      comment.user.avatar
+                        ? comment.user.avatar.secure_url
+                        : null
+                    }
+                    aria-label="Recipe"
+                    className={classes.avatar}
+                  />
+                </Link>
+              ) : (
+                <Link to={`/${comment.user.username}`}>
+                  <Avatar aria-label="Recipe" className={classes.avatar}>
+                    {comment.user.username.substring(0, 1)}
+                  </Avatar>
+                </Link>
+              )
+            }
+            action={
+              <IconButton aria-label="Settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={
+              <Link to={`/${comment.user.username}`}>
+                {comment.user.username}
+              </Link>
+            }
+            subheader={
+              <Moment fromNow locale="de">
+                {comment.dateCreated}
+              </Moment>
+            }
+          />
           <Grid
             container
             direction="row"
             justify="center"
             alignItems="flex-start"
           >
-            <Grid item>
-              <Grid
-                container
-                direction="row"
-                justify="space-between"
-                alignItems="center"
-                className={classes.header}
-              >
-                <Grid item>
-                  <Grid
-                    container
-                    direction="row"
-                    justify="space-between"
-                    alignItems="center"
-                  >
-                    <CommentFeedItemAvatar comment={comment} />
-                    <Grid>
-                      <CommentFeedItemCreator comment={comment} />
-                      <CommentFeedItemDate comment={comment} />
-                    </Grid>
-                  </Grid>
-                </Grid>
-
-                <Grid item>
-                  {comment.user._id === auth.user.id ? (
-                    <CommentFeedItemButtons
-                      comment={comment}
-                      onDeleteClick={onDeleteClick}
-                      onEditClick={onEditClick}
-                    />
-                  ) : null}
-                </Grid>
-              </Grid>
+            <CardContent>
               <CommentFeedItemText comment={comment} />
-            </Grid>
+            </CardContent>
           </Grid>
-        </CardContent>
+        </React.Fragment>
       ) : (
         <CardContent>
           <CommentEditContainer comment={comment} onSaveClick={onSaveClick} />
         </CardContent>
       )}
       <Divider />
-      <CardContent className={classes.buttonContainer}>
+
+      <CardActions disableSpacing>
         <CommentFeedItemVote
           comment={comment}
           commentsByPostRef={commentsByPostRef}
           setCommentsByPostRef={setCommentsByPostRef}
         />
-      </CardContent>
+      </CardActions>
+
       <Divider />
       {subComments.length ? (
         <List className={classes.root}>
