96acad01859d0528d7182ef99cb2385a8742d08f
‘axzilla’ <chanter-acidic-0r@icloud.com>
2019-06-08 13:09:36 +0200

    Refactoring on isEmpty.js

--- packages/app/src/utils/isEmpty.js
+++ packages/app/src/utils/isEmpty.js
@@ -1,7 +1,8 @@
-const isEmpty = value =>
+function isEmpty(value) {
   value === undefined ||
-  value === null ||
-  (typeof value === 'object' && Object.keys(value).length === 0) ||
-  (typeof value === 'string' && value.trim().length === 0)
+    value === null ||
+    (typeof value === 'object' && Object.keys(value).length === 0) ||
+    (typeof value === 'string' && value.trim().length === 0)
+}
 
 export default isEmpty
