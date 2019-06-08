57c56befa129fe4276adc6196d286a1f90431168
Axel <axel.adrian@me.com>
2019-06-08 13:10:27 +0200

    Refactoring on setAuthToken.js

--- packages/app/src/utils/setAuthToken.js
+++ packages/app/src/utils/setAuthToken.js
@@ -1,13 +1,11 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const setAuthToken = token => {
+function setAuthToken(token) {
   if (token) {
-    // Apply to every request
-    axios.defaults.headers.common['Authorization'] = token;
+    axios.defaults.headers.common['Authorization'] = token
   } else {
-    // Delete auth header
-    delete axios.defaults.headers.common['Authorization'];
+    delete axios.defaults.headers.common['Authorization']
   }
-};
+}
 
-export default setAuthToken;
+export default setAuthToken
