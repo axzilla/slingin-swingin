b381c81627eaabd045e4c582e500e53a6d83319a
‘axzilla’ <chanter-acidic-0r@icloud.com>
2019-12-30 11:39:53 +0700

    Install GA and get it up running initialy on home

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -5,6 +5,7 @@ dotenv.config()
 
 module.exports = withImages({
   env: {
-    CODEHUSTLA_API_URL: process.env.CODEHUSTLA_API_URL
+    CODEHUSTLA_API_URL: process.env.CODEHUSTLA_API_URL,
+    CODEHUSTLA_GOOGLE_ANALYTICS: process.env.CODEHUSTLA_GOOGLE_ANALYTICS
   }
 })
