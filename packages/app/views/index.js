410fee81cde3719f0cdba507923e50015eecf3d3
Axel <axel.adrian@me.com>
2019-12-31 17:43:03 +0700

    Refactoring on PostEdit

--- packages/app/views/index.js
+++ packages/app/views/index.js
@@ -1,3 +1,4 @@
 export { default as PostDetails } from './PostDetails'
 export { default as PostCreate } from './PostCreate'
 export { default as PostFeedByTag } from './PostFeedByTag'
+export { default as PostEdit } from './PostEdit'
