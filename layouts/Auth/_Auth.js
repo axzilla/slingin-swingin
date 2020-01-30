285f6c3a8bf267fbf17c5c4f852ad4d1efb935ef
Axel <axel.adrian@me.com>
2020-01-30 18:17:22 +0700

    Finish issue

--- layouts/Auth/_Auth.js
+++ layouts/Auth/_Auth.js
@@ -8,6 +8,7 @@ import NextLink from '../../components/NextLink'
 import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
+import Container from '@material-ui/core/Container'
 
 const useStyles = makeStyles(theme => ({
   root: { padding: theme.spacing(5, 1, 0) },
@@ -18,12 +19,20 @@ function Auth({ children }) {
   const classes = useStyles()
 
   return (
-    <Grid className={classes.root} container alignItems="center" direction="column">
-      <NextLink href="/">
-        <img className={classes.logo} src={logo} />
-      </NextLink>
-      {children}
-    </Grid>
+    <Container component="main" maxWidth="xs">
+      <Grid
+        style={{ width: '100%' }}
+        className={classes.root}
+        container
+        alignItems="center"
+        direction="column"
+      >
+        <NextLink href="/">
+          <img className={classes.logo} src={logo} />
+        </NextLink>
+        {children}
+      </Grid>
+    </Container>
   )
 }
 
