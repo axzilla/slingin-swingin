aa48a912edf1864c51d87057d6f884bb5276acf3
‘axzilla’ <chanter-acidic-0r@icloud.com>
2020-01-31 16:41:39 +0700

    Bring back alert

--- layouts/Main/_Main.js
+++ layouts/Main/_Main.js
@@ -1,27 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-// import Topbar from './components/Topbar'
 import Footer from './components/Footer'
-
 import Topbar from '../../components/Topbar'
 import TopbarMixings from '../../components/TopbarMixings'
-import Alert from '../../components/Alert'
 
 import Grid from '@material-ui/core/Grid'
 import Container from '@material-ui/core/Container'
 
 function Main({ children }) {
   return (
-    <>
-      <Grid container>
-        <Topbar />
-        <TopbarMixings />
-        <Container>{children}</Container>
-        <Footer />
-      </Grid>
-      <Alert />
-    </>
+    <Grid container>
+      <Topbar />
+      <TopbarMixings />
+      <Container>{children}</Container>
+      <Footer />
+    </Grid>
   )
 }
 
