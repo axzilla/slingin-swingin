0b96cc8308d838170745c3261ec649ca615a925b
Axel <axel.adrian@me.com>
2020-02-05 18:06:31 +0700

    Fix dangerouslySetInnerHTML issue

--- views/PostDetails/components/Content/_Content.js
+++ views/PostDetails/components/Content/_Content.js
@@ -3,10 +3,8 @@ import PropTypes from 'prop-types'
 
 import htmlToMui from '@utils/htmlToMui'
 
-import Typography from '@material-ui/core/Typography'
-
 function PostDeatilsItemContent({ post }) {
-  return <Typography dangerouslySetInnerHTML={{ __html: htmlToMui(post.text) }} />
+  return <div dangerouslySetInnerHTML={{ __html: htmlToMui(post.text) }} />
 }
 
 PostDeatilsItemContent.propTypes = {
