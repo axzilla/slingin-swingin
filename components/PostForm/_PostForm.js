7fa337151f2cd604baf582616157f4df90ae4ce0
Axel <axel.adrian@me.com>
2020-02-06 15:04:57 +0700

    Create ProfileForm instead of ProfileCreate and ProfileEdit

---
+++ components/PostForm/_PostForm.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import Router from 'next/router'
+
+import Title from './components/Title'
+import TitleImage from './components/TitleImage'
+import Tags from './components/Tags'
+import Story from './components/Story'
+
+import { postCreate, postUpdate } from '@services/post'
+import Container from '@components/Container'
+
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+
+function PostForm({ post }) {
+  const [errors, setErrors] = useState()
+  const [titleImage, setTitleImage] = useState(null)
+  const [titleImagePreview, setTitleImagePreview] = useState(
+    post && post.titleImage ? post.titleImage.secure_url : null
+  )
+  const [title, setTitle] = useState(post ? post.title : '')
+  const [text, setText] = useState(post ? post.text : '')
+  const [tags, setTags] = useState(post ? post.tags : [])
+  const [tagsInput, setTagsInput] = useState('')
+
+  async function onSubmit() {
+    try {
+      const formData = new FormData()
+
+      formData.append('titleImage', titleImage)
+      formData.append('title', title)
+      formData.append('text', text)
+      formData.append('tags', tags)
+
+      if (post) {
+        formData.append('id', post._id)
+      }
+
+      if (post) {
+        const res = await postUpdate(formData)
+        const updatedPost = res.data
+        const { shortId, urlSlug } = updatedPost
+
+        Router.push(`/post/${shortId}/${urlSlug}`)
+      } else if (!post) {
+        const res = await postCreate(formData)
+        const createdPost = res.data
+        const { shortId, urlSlug } = createdPost
+
+        Router.push(`/post/${shortId}/${urlSlug}`)
+      }
+    } catch (error) {
+      setErrors(error.response.data)
+    }
+  }
+
+  return (
+    <Container maxWidth="md">
+      <Grid container direction="column" spacing={2}>
+        <Grid item>
+          <TitleImage
+            setTitleImage={setTitleImage}
+            titleImagePreview={titleImagePreview}
+            setTitleImagePreview={setTitleImagePreview}
+          />
+        </Grid>
+        <Grid item>
+          <Title title={title} setTitle={setTitle} errors={errors} />
+        </Grid>
+        <Grid item>
+          <Story text={text} setText={setText} errors={errors} />
+        </Grid>
+        <Grid item>
+          <Tags
+            tags={tags}
+            setTags={setTags}
+            tagsInput={tagsInput}
+            setTagsInput={setTagsInput}
+            errors={errors}
+          />
+        </Grid>
+        <Grid item>
+          <Button color="secondary" variant="contained" onClick={onSubmit} fullWidth>
+            Save
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+PostForm.propTypes = {
+  post: PropTypes.object
+}
+
+export default PostForm
---
+++ components/PostForm/components/Story/_Story.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Quill from '@components/Quill'
+
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardHeader from '@material-ui/core/CardHeader'
+
+import Divider from '@material-ui/core/Divider'
+
+function PostForm({ text, setText, errors }) {
+  async function onTextChange(value) {
+    // value instead event.target.value - is quill specified
+    setText(value)
+  }
+
+  return (
+    <Card>
+      <CardHeader title="Post Story" />
+      <Divider />
+      <CardContent>
+        <div>
+          <Quill
+            error={errors && errors.text}
+            value={text}
+            onChange={onTextChange}
+            placeholder="Write your story..."
+          />
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+
+PostForm.propTypes = {
+  text: PropTypes.string,
+  setText: PropTypes.func,
+  errors: PropTypes.object
+}
+
+export default PostForm
