f8b3840284e81d13ab29da02ba56b180b9b0335a
Axel <axel.adrian@me.com>
2020-02-09 18:09:45 +0700

    Use Box to order MainLayout

--- layouts/Main/_Main.js
+++ layouts/Main/_Main.js
@@ -9,6 +9,7 @@ import WidgetLatestUsers from '@components/WidgetLatestUsers'
 import WidgetTopPostsTags from '@components/WidgetTopPostsTags'
 
 import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
 
 function Main({ children }) {
   return (
@@ -16,15 +17,23 @@ function Main({ children }) {
       <Topbar />
       <Container maxWidth="lg">
         <Grid container direction="row" justify="center" alignItems="flex-start" spacing={2}>
-          <Grid item xs={12} md={3}>
-            <WidgetTopPostsTags />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            {children}
-          </Grid>
-          <Grid item xs={12} md={3}>
-            <WidgetLatestUsers />
-          </Grid>
+          <Box clone order={{ xs: 2, md: 1 }}>
+            <Grid item xs={12} md={3}>
+              <WidgetTopPostsTags />
+            </Grid>
+          </Box>
+
+          <Box clone order={{ xs: 1, md: 2 }}>
+            <Grid item xs={12} md={6}>
+              {children}
+            </Grid>
+          </Box>
+
+          <Box clone order={{ xs: 3, md: 3 }}>
+            <Grid item xs={12} md={3}>
+              <WidgetLatestUsers />
+            </Grid>
+          </Box>
         </Grid>
       </Container>
       <Footer />
