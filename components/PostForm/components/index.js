e4f13e677d630a41d6a2f1f183c3c12b6cae94c7
‘axzilla’ <chanter-acidic-0r@icloud.com>
2020-02-09 13:08:04 +0700

    Change wording sory to content

--- components/PostForm/components/index.js
+++ components/PostForm/components/index.js
@@ -1,4 +1,4 @@
 export { default as Title } from './Title'
 export { default as TitleImage } from './TitleImage'
 export { default as Tags } from './Tags'
-export { default as Story } from './Story'
+export { default as Content } from './Content'
