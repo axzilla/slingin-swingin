debc4bba9a811b015fe1b4bff982a1c663225021
‘axzilla’ <chanter-acidic-0r@icloud.com>
2020-02-09 13:06:22 +0700

    Add markdownToHtml on PostDetails content

--- views/PostDetails/components/Content/_Content.js
+++ views/PostDetails/components/Content/_Content.js
@@ -2,9 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import htmlToMui from '@utils/htmlToMui'
+import markdownToHtml from '@utils/markdownToHtml'
 
 function PostDeatilsItemContent({ post }) {
-  return <div dangerouslySetInnerHTML={{ __html: htmlToMui(post.content) }} />
+  return <div dangerouslySetInnerHTML={{ __html: htmlToMui(markdownToHtml(post.content)) }} />
 }
 
 PostDeatilsItemContent.propTypes = {
