56e9ca32af3015ccbd22be328099db1a52ace6f1
‘axzilla’ <chanter-acidic-0r@icloud.com>
2020-02-10 10:57:01 +0700

    Redesign AuthModal

--- components/AuthModal/_AuthModal.js
+++ components/AuthModal/_AuthModal.js
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react'
 
 import AuthContext from '@contexts/AuthContext'
+import Link from '@components/Link'
 
+import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
-import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
+import Typography from '@material-ui/core/Typography'
 
 function AuthModal() {
   const { isAuthModal, setIsAuthModal } = useContext(AuthContext)
@@ -24,22 +26,36 @@ function AuthModal() {
       aria-describedby="alert-dialog-description"
     >
       <DialogTitle id="alert-dialog-title">
-        This feature is for registered bounce.dev members only!
+        <Typography variant="h4" align="center">
+          NOT A MEMBER?
+        </Typography>
       </DialogTitle>
       <DialogContent>
-        <DialogContentText id="alert-dialog-description">
+        <DialogContentText id="alert-dialog-description" align="center">
           This feature is for registered bounce.dev members only.
-          <br />
-          <br />
+        </DialogContentText>
+        <Box my={2}>
+          <Link href="/register">
+            <Button onClick={handleClose} variant="contained" fullWidth color="primary">
+              Sign Up
+            </Button>
+          </Link>
+          <Box my={1}>
+            <Typography variant="h5" align="center">
+              or
+            </Typography>
+          </Box>
+          <Link href="/login">
+            <Button onClick={handleClose} variant="outlined" fullWidth color="primary">
+              Log In
+            </Button>
+          </Link>
+        </Box>
+        <DialogContentText id="alert-dialog-description" align="center">
           Log in to your account or create an account for FREE if you haven`t done so and join the
           coolest community of music lovers that are already on bounce.dev!
         </DialogContentText>
       </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Cancel
-        </Button>
-      </DialogActions>
     </Dialog>
   )
 }
