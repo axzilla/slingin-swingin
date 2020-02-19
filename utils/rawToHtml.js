e829d721d29b34c86cb2f747297dd9fde81768bf
Axel <axel.adrian@me.com>
2020-02-19 09:48:20 +0700

    Style first block renderer

--- utils/rawToHtml.js
+++ utils/rawToHtml.js
@@ -1,10 +1,22 @@
 import { stateToHTML } from 'draft-js-export-html'
 import { convertFromRaw } from 'draft-js'
 
+let options = {
+  blockRenderers: {
+    unstyled: block => {
+      if (block.getText()) {
+        return `<p class="MuiTypography-root MuiTypography-body1">${block.getText()}</p>`
+      } else {
+        return '<p class="MuiTypography-root MuiTypography-body1"><br></p>'
+      }
+    }
+  }
+}
+
 function rawToHtml(content) {
   const parsedJson = JSON.parse(content)
   const convertedRaw = convertFromRaw(parsedJson)
-  const html = stateToHTML(convertedRaw)
+  const html = stateToHTML(convertedRaw, options)
 
   return html
 }
