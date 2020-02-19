178437539e048f640cb84d7e38ff70d31e600b8c
Axel <axel.adrian@me.com>
2020-02-19 09:07:12 +0700

    Add richutils

--- components/EditorPost/_EditorPost.js
+++ components/EditorPost/_EditorPost.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import dynamic from 'next/dynamic'
-import { EditorState, convertToRaw, convertFromRaw } from 'draft-js'
+import { EditorState, RichUtils, convertToRaw, convertFromRaw } from 'draft-js'
 const Editor = dynamic(() => import('draft-js').then(mod => mod.Editor), { ssr: false })
 
 import { makeStyles } from '@material-ui/styles'
@@ -55,10 +55,47 @@ function EditorPost({ content, setContent, placeholder }) {
     setContent(convertToRaw(editorState.getCurrentContent()))
   }
 
+  function handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(editorState, command)
+
+    if (newState) {
+      onChange(newState)
+      return 'handled'
+    }
+
+    return 'not-handled'
+  }
+
+  function onUnderlineClick() {
+    onChange(RichUtils.toggleInlineStyle(editorState, 'UNDERLINE'))
+  }
+
+  function onBoldClick() {
+    onChange(RichUtils.toggleInlineStyle(editorState, 'BOLD'))
+  }
+
+  function onItalicClick() {
+    onChange(RichUtils.toggleInlineStyle(editorState, 'ITALIC'))
+  }
+
   return (
-    <div className={classes.wrapper}>
-      <Editor editorState={editorState} onChange={onChange} placeholder={placeholder} />
-    </div>
+    <>
+      <button onClick={onUnderlineClick}>U</button>
+      <button onClick={onBoldClick}>
+        <b>B</b>
+      </button>
+      <button onClick={onItalicClick}>
+        <em>I</em>
+      </button>
+      <div className={classes.wrapper}>
+        <Editor
+          editorState={editorState}
+          onChange={onChange}
+          handleKeyCommand={handleKeyCommand}
+          placeholder={placeholder}
+        />
+      </div>
+    </>
   )
 }
 
