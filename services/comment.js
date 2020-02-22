877e0598a077f32d174ba30beb28ce1cd681c7dd
Axel <axel.adrian@me.com>
2020-02-22 12:57:56 +0700

    Make changes

--- services/comment.js
+++ services/comment.js
@@ -1,31 +1,31 @@
 import axios from 'axios'
 
-const rootUrl = `${process.env.NOIZE_APP_API_URL}/comments`
+const serverUrl = `${process.env.NOIZE_APP_SERVER_URL}/comments`
 
 export function commentCreate(commentData) {
-  return axios.post(`${rootUrl}/comment-create`, commentData)
+  return axios.post(`${serverUrl}/comment-create`, commentData)
 }
 
 export function commentUpdate(commentData) {
-  return axios.patch(`${rootUrl}/comment-update`, commentData)
+  return axios.patch(`${serverUrl}/comment-update`, commentData)
 }
 
 export function commentDelete(commentId) {
-  return axios.delete(`${rootUrl}/comment-delete`, { data: { commentId } })
+  return axios.delete(`${serverUrl}/comment-delete`, { data: { commentId } })
 }
 
 export function commentUpvote(commentId) {
-  return axios.post(`${rootUrl}/comment-upvote/${commentId}`)
+  return axios.post(`${serverUrl}/comment-upvote/${commentId}`)
 }
 
 export function commentDownvote(commentId) {
-  return axios.post(`${rootUrl}/comment-downvote/${commentId}`)
+  return axios.post(`${serverUrl}/comment-downvote/${commentId}`)
 }
 
 export function getCommentsByPostRef(postRef) {
-  return axios.get(`${rootUrl}/get-comments-by-post-ref/${postRef}`)
+  return axios.get(`${serverUrl}/get-comments-by-post-ref/${postRef}`)
 }
 
 export function getCommentsByUserId(userId) {
-  return axios.get(`${rootUrl}/get-comments-by-user-id/${userId}`)
+  return axios.get(`${serverUrl}/get-comments-by-user-id/${userId}`)
 }
