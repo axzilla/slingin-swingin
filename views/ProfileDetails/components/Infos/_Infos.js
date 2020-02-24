e72721ae4d4658981e599f17ad21e6f64214c625
Axel <axel.adrian@me.com>
2020-02-24 17:15:19 +0700

    Make translations

--- views/ProfileDetails/components/Infos/_Infos.js
+++ views/ProfileDetails/components/Infos/_Infos.js
@@ -50,7 +50,7 @@ function ProfileDetailsInfos({ profile }) {
       {isEmpty(profile.location) ? null : (
         <Grid item xs={12} sm={6} md={3} className={classes.infoContainer}>
           <Typography variant="subtitle1" className={classes.infoKey}>
-            kommt aus
+            from
           </Typography>
           <Typography variant="subtitle1" className={classes.infoValue}>
             {profile.location}
@@ -59,7 +59,7 @@ function ProfileDetailsInfos({ profile }) {
       )}
       <Grid item xs={12} sm={6} md={3} className={classes.infoContainer}>
         <Typography variant="subtitle1" className={classes.infoKey}>
-          beigetreten
+          joined
         </Typography>
         <Typography variant="subtitle1" className={classes.infoValue}>
           <Moment fromNow>{profile.dateCreated}</Moment>
