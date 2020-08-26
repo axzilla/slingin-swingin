610ae023fd4f4f2843a32d795fddd9263de99031
Axel <axel.adrian@me.com>
2020-08-26 16:21:59 +0200

    Remove test code

--- views/Register/_Register.js
+++ views/Register/_Register.js
@@ -14,7 +14,7 @@ const Register = () => {
   const [errors, setErrors] = useState('')
 
   const [registerData, setRegisterData] = useState({
-    email: 'a',
+    email: '',
     password: '',
     username: ''
   })
