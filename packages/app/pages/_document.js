d89db87239e71b53902670c9807c34ef03c22bb2
‘axzilla’ <chanter-acidic-0r@icloud.com>
2020-09-10 09:12:17 +0000

    Show guests connections on server logs, remove withCSS package and setup, update next and react packages, create socket.io context provider, remove socket.io setup from _app.js, redesign _document.js, move index.js to pages root folder

--- packages/app/pages/_document.js
+++ packages/app/pages/_document.js
@@ -1,22 +1,13 @@
 import React from 'react'
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 import { ServerStyleSheets } from '@material-ui/styles'
 
 class MyDocument extends Document {
   render() {
     return (
-      <html lang="de" dir="ltr">
+      <Html lang="de" dir="ltr">
         <Head>
-          <meta charSet="utf-8" />
-          {/* Use minimum-scale=1 to enable GPU rasterization */}
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-          />
-          {/* PWA primary color */}
-          {/* <meta name="theme-color" content={theme.palette.primary.main} /> */}
-
           <link
             href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700&display=swap"
             rel="stylesheet"
@@ -28,7 +19,7 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
