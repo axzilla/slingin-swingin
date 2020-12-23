e0a9e41400fd1c9f7a7b92e89b39d7b8361a8aed
‘axzilla’ <chanter-acidic-0r@icloud.com>
2020-12-23 18:04:47 +0100

    Initial commit for app idea improving

--- packages/api/nodemailer/transporter.js
+++ packages/api/nodemailer/transporter.js
@@ -1,11 +1,24 @@
 const nodemailer = require('nodemailer')
-const mg = require('nodemailer-mailgun-transport')
 
-const auth = {
+module.exports = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
-    api_key: process.env.MAILGUN_API_KEY,
-    domain: process.env.MAILGUN_DOMAIN
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASSWORD
   }
-}
+})
 
-module.exports = nodemailer.createTransport(mg(auth))
+// ### MAILGUN STUFF ###
+// const nodemailer = require('nodemailer')
+// const mg = require('nodemailer-mailgun-transport')
+
+// const auth = {
+//   auth: {
+//     api_key: process.env.MAILGUN_API_KEY,
+//     domain: process.env.MAILGUN_DOMAIN
+//   }
+// }
+
+// module.exports = nodemailer.createTransport(mg(auth))
