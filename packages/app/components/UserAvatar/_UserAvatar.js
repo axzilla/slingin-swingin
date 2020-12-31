e09d556d5131890011e2d02bdf7a41df90e25adf
‘axzilla’ <chanter-acidic-0r@icloud.com>
2020-12-31 15:57:36 +0100

    Comment out all isOnline stuff

--- packages/app/components/UserAvatar/_UserAvatar.js
+++ packages/app/components/UserAvatar/_UserAvatar.js
@@ -3,50 +3,51 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 // MUI
-import Badge from '@material-ui/core/Badge'
+// import Badge from '@material-ui/core/Badge'
+// import { makeStyles, withStyles } from '@material-ui/core/styles'
 import Avatar from '@material-ui/core/Avatar'
-import { makeStyles, withStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles({
   avatar: { height: ({ height }) => `${height}px`, width: ({ width }) => `${width}px` }
 })
 
 function UserAvatar({ user, height, width }) {
-  const StyledBadge = withStyles(theme => ({
-    badge: {
-      backgroundColor: user.isOnline ? '#44b700' : 'red',
-      color: user.isOnline ? '#44b700' : 'red',
-      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-      '&::after': {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        borderRadius: '50%',
-        border: '1px solid currentColor',
-        content: '""'
-      }
-    }
-  }))(Badge)
+  // const StyledBadge = withStyles(theme => ({
+  //   badge: {
+  //     backgroundColor: user.isOnline ? '#44b700' : 'red',
+  //     color: user.isOnline ? '#44b700' : 'red',
+  //     boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+  //     '&::after': {
+  //       position: 'absolute',
+  //       top: 0,
+  //       left: 0,
+  //       width: '100%',
+  //       height: '100%',
+  //       borderRadius: '50%',
+  //       border: '1px solid currentColor',
+  //       content: '""'
+  //     }
+  //   }
+  // }))(Badge)
 
   const classes = useStyles({ height, width })
 
   return (
-    <StyledBadge
-      overlap="circle"
-      anchorOrigin={{
-        vertical: 'bottom',
-        horizontal: 'right'
-      }}
-      variant="dot"
-    >
-      <Avatar
-        className={classes.avatar}
-        alt={user.username.toUpperCase()}
-        src={(user.avatar && user.avatar.secure_url) || '/broken-image.jpg'}
-      />
-    </StyledBadge>
+    // <StyledBadge
+    //   overlap="circle"
+    //   anchorOrigin={{
+    //     vertical: 'bottom',
+    //     horizontal: 'right'
+    //   }}
+    //   variant="dot"
+    // >
+    <Avatar
+      className={classes.avatar}
+      alt={user.username.toUpperCase()}
+      src={(user.avatar && user.avatar.secure_url) || '/broken-image.jpg'}
+    />
+    // </StyledBadge>
   )
 }
 
