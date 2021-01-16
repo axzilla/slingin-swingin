443ac522ac3fd9d0ef29a30f19bc3adb7743aef1
Axel <axel.adrian@me.com>
2021-01-16 11:54:44 +0100

    Integrate review stepper

--- packages/app/views/PlaceDetails/components/ReviewCreateOrUpdate/_ReviewCreateOrUpdate.js
+++ packages/app/views/PlaceDetails/components/ReviewCreateOrUpdate/_ReviewCreateOrUpdate.js
@@ -3,12 +3,11 @@ import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 // Local Components
-import { RatingItem } from './components'
+import { Ratings, Text } from './components'
 
 // MUI
 import { useTheme } from '@material-ui/core/styles'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
-import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
@@ -19,43 +18,11 @@ import DialogTitle from '@material-ui/core/DialogTitle'
 import Box from '@material-ui/core/Box'
 import TextField from '@material-ui/core/TextField'
 import InputAdornment from '@material-ui/core/InputAdornment'
-//
 import { makeStyles } from '@material-ui/core/styles'
-import Stepper from '@material-ui/core/Stepper'
-import Step from '@material-ui/core/Step'
-import StepLabel from '@material-ui/core/StepLabel'
 
-//
-function getSteps() {
-  return ['Select campaign settings', 'Create an ad group', 'Create an ad']
-}
-
-function getStepContent(step) {
-  switch (step) {
-    case 0:
-      return 'Select campaign settings...'
-    case 1:
-      return 'What is an ad group anyways?'
-    case 2:
-      return 'This is the bit I really care about!'
-    default:
-      return 'Unknown step'
-  }
-}
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%'
-  },
-  button: {
-    marginRight: theme.spacing(1)
-  },
-  instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1)
-  }
-}))
-//
+const useStyles = makeStyles({
+  dialogPaper: { height: '100%' }
+})
 
 function ReviewCreateOrUpdate({
   ratings,
@@ -66,6 +33,7 @@ function ReviewCreateOrUpdate({
   setPlaceReview,
   baseData
 }) {
+  const classes = useStyles()
   const theme = useTheme()
   const fullScreen = useMediaQuery(theme.breakpoints.down('sm'))
   const [open, setOpen] = useState(false)
@@ -76,6 +44,11 @@ function ReviewCreateOrUpdate({
 
   const handleClose = () => {
     setOpen(false)
+
+    // design hack because flittering
+    setTimeout(() => {
+      handleReset()
+    }, 100)
   }
 
   function handleChangePlaceReview(event) {
@@ -85,7 +58,7 @@ function ReviewCreateOrUpdate({
   async function handleCreateOrUpdatePlaceReview() {
     try {
       userReview ? handleUpdatePlaceReview() : handleCreatePlaceReview()
-      setOpen(false)
+      handleClose()
     } catch (error) {
       if (error) throw error
     }
@@ -97,104 +70,26 @@ function ReviewCreateOrUpdate({
     }
   }
 
-  //
-  const classes = useStyles()
-
-  const [activeStep, setActiveStep] = useState(0)
-  const [skipped, setSkipped] = useState(new Set())
-  const steps = getSteps()
-
-  const isStepOptional = step => {
-    return step === 1
+  function getSteps() {
+    return ['text', 'ratings', 'costs']
   }
 
-  const isStepSkipped = step => {
-    return skipped.has(step)
-  }
-
-  const handleNext = () => {
-    let newSkipped = skipped
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values())
-      newSkipped.delete(activeStep)
-    }
-
-    setActiveStep(prevActiveStep => prevActiveStep + 1)
-    setSkipped(newSkipped)
-  }
-
-  const handleBack = () => {
-    setActiveStep(prevActiveStep => prevActiveStep - 1)
-  }
-
-  const handleSkip = () => {
-    if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
-      throw new Error("You can't skip a step that isn't optional.")
-    }
-
-    setActiveStep(prevActiveStep => prevActiveStep + 1)
-    setSkipped(prevSkipped => {
-      const newSkipped = new Set(prevSkipped.values())
-      newSkipped.add(activeStep)
-      return newSkipped
-    })
-  }
-
-  const handleReset = () => {
-    setActiveStep(0)
-  }
-  //
-
-  return (
-    <>
-      <Box mb={2}>
-        <Grid container justify="flex-end">
-          <Button size="large" variant="outlined" onClick={handleClickOpen}>
-            {userReview ? 'Edit' : 'Write'} Review
-          </Button>
-        </Grid>
-      </Box>
-      <Dialog
-        fullScreen={fullScreen}
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="responsive-dialog-title"
-      >
-        <DialogTitle id="responsive-dialog-title">
-          {userReview ? 'Edit' : 'Write'} Review for {baseData.mapBox.place_name}
-        </DialogTitle>
-        <Divider />
-        <DialogContent>
-          <Box mb={2}>
-            <TextField
-              name="text"
-              fullWidth
-              variant="outlined"
-              value={placeReview.text}
-              onChange={handleChangePlaceReview}
-              multiline
-              rows={6}
-            />
-          </Box>
-          <Grid spacing={2} container>
-            {ratings.map((rating, index) => {
-              return (
-                <Grid item xs={6} key={index}>
-                  <Typography>{rating.label}</Typography>
-                  <RatingItem
-                    rating={rating}
-                    placeReview={placeReview}
-                    setPlaceReview={setPlaceReview}
-                    handleChangePlaceReview={handleChangePlaceReview}
-                  />
-                </Grid>
-              )
-            })}
-            <Grid item xs={12}>
-              <Divider />
-            </Grid>
+  function getStepContent(step) {
+    switch (step) {
+      case 0:
+        return <Text placeReview={placeReview} handleChangePlaceReview={handleChangePlaceReview} />
+      case 1:
+        return (
+          <Ratings
+            ratings={ratings}
+            placeReview={placeReview}
+            setPlaceReview={setPlaceReview}
+            handleChangePlaceReview={handleChangePlaceReview}
+          />
+        )
+      case 2:
+        return (
+          <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
                 type="number"
@@ -230,88 +125,110 @@ function ReviewCreateOrUpdate({
               />
             </Grid>
           </Grid>
-        </DialogContent>
+        )
+    }
+  }
+
+  const [activeStep, setActiveStep] = useState(0)
+  const [skipped, setSkipped] = useState(new Set())
+  const steps = getSteps()
+
+  const isStepOptional = step => {
+    return step === 0 || step === 1
+  }
+
+  const isStepSkipped = step => {
+    return skipped.has(step)
+  }
+
+  const handleNext = () => {
+    let newSkipped = skipped
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values())
+      newSkipped.delete(activeStep)
+    }
+
+    setActiveStep(prevActiveStep => prevActiveStep + 1)
+    setSkipped(newSkipped)
+  }
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1)
+  }
+
+  const handleSkip = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1)
+    setSkipped(prevSkipped => {
+      const newSkipped = new Set(prevSkipped.values())
+      newSkipped.add(activeStep)
+      return newSkipped
+    })
+  }
+
+  const handleReset = () => {
+    setActiveStep(0)
+  }
+  //
+
+  return (
+    <>
+      <Box mb={2}>
+        <Grid container justify="flex-end">
+          <Button size="large" variant="outlined" onClick={handleClickOpen}>
+            {userReview ? 'Edit' : 'Write'} Review
+          </Button>
+        </Grid>
+      </Box>
+      <Dialog
+        fullScreen={fullScreen}
+        fullWidth
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="responsive-dialog-title"
+        classes={{ paper: classes.dialogPaper }}
+      >
+        <DialogTitle id="responsive-dialog-title">
+          {userReview ? 'Edit' : 'Write'} Review for {baseData.mapBox.place_name}
+        </DialogTitle>
+        <Divider />
+        <DialogContent>{getStepContent(activeStep)}</DialogContent>
         <Divider />
         <DialogActions>
-          <Button onClick={handleClose} variant="outlined">
-            Close
-          </Button>
-          <Button
-            disabled={isDisabled()}
-            onClick={handleCreateOrUpdatePlaceReview}
-            variant="outlined"
-            color="secondary"
-          >
-            {userReview ? 'Save' : 'Create'}
-          </Button>
-        </DialogActions>
-        {/*  */}
-        <div className={classes.root}>
-          <Stepper activeStep={activeStep}>
-            {steps.map((label, index) => {
-              const stepProps = {}
-              const labelProps = {}
-              if (isStepOptional(index)) {
-                labelProps.optional = <Typography variant="caption">Optional</Typography>
-              }
-              if (isStepSkipped(index)) {
-                stepProps.completed = false
-              }
-              return (
-                <Step key={label} {...stepProps}>
-                  <StepLabel {...labelProps}>{label}</StepLabel>
-                </Step>
-              )
-            })}
-          </Stepper>
-          <div>
+          <>
             {activeStep === steps.length ? (
-              <div>
-                <Typography className={classes.instructions}>
-                  All steps completed - you&apos;re finished
-                </Typography>
-                <Button onClick={handleReset} className={classes.button}>
+              <>
+                <Button variant="outlined" onClick={handleReset}>
                   Reset
                 </Button>
-              </div>
+              </>
             ) : (
-              <div>
-                <Typography className={classes.instructions}>
-                  {getStepContent(activeStep)}
-                </Typography>
-                <div>
-                  <Button
-                    disabled={activeStep === 0}
-                    onClick={handleBack}
-                    className={classes.button}
-                  >
-                    Back
+              <>
+                <Button variant="outlined" disabled={activeStep === 0} onClick={handleBack}>
+                  Back
+                </Button>
+                {isStepOptional(activeStep) && (
+                  <Button variant="outlined" color="secondary" onClick={handleSkip}>
+                    Skip
                   </Button>
-                  {isStepOptional(activeStep) && (
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={handleSkip}
-                      className={classes.button}
-                    >
-                      Skip
-                    </Button>
-                  )}
-
+                )}
+                {activeStep === steps.length - 1 ? (
                   <Button
+                    disabled={isDisabled()}
+                    onClick={handleCreateOrUpdatePlaceReview}
                     variant="contained"
-                    color="primary"
-                    onClick={handleNext}
-                    className={classes.button}
+                    color="secondary"
                   >
-                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                    Finish
                   </Button>
-                </div>
-              </div>
+                ) : (
+                  <Button variant="contained" color="secondary" onClick={handleNext}>
+                    Next
+                  </Button>
+                )}
+              </>
             )}
-          </div>
-        </div>
-        {/*  */}
+          </>
+        </DialogActions>
       </Dialog>
     </>
   )
---
+++ packages/app/views/PlaceDetails/components/ReviewCreateOrUpdate/components/Ratings/_Ratings.js
@@ -0,0 +1,38 @@
+// Packages
+import PropTypes from 'prop-types'
+
+// Local Components
+import { RatingItem } from './components'
+
+// MUI
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+
+function Ratings({ ratings, placeReview, setPlaceReview, handleChangePlaceReview }) {
+  return (
+    <Grid container spacing={2}>
+      {ratings.map((rating, index) => {
+        return (
+          <Grid item xs={6} key={index}>
+            <Typography>{rating.label}</Typography>
+            <RatingItem
+              rating={rating}
+              placeReview={placeReview}
+              setPlaceReview={setPlaceReview}
+              handleChangePlaceReview={handleChangePlaceReview}
+            />
+          </Grid>
+        )
+      })}
+    </Grid>
+  )
+}
+
+Ratings.propTypes = {
+  ratings: PropTypes.array.isRequired,
+  placeReview: PropTypes.object.isRequired,
+  setPlaceReview: PropTypes.func.isRequired,
+  handleChangePlaceReview: PropTypes.func.isRequired
+}
+
+export default Ratings
