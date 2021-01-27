0d61fc175bea8b34cd09e3700f8d23b4cc7a4920
Axel <axel.adrian@me.com>
2021-01-27 16:37:43 +0100

    Add GA

--- packages/app/pages/_document.js
+++ packages/app/pages/_document.js
@@ -14,6 +14,23 @@ class MyDocument extends Document {
           />
 
           <link rel="stylesheet" href="/style.css" />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+            }}
+          />
         </Head>
         <body>
           <Main />
