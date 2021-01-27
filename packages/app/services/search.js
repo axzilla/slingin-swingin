ac21f33913eb5e64c587942327b9843cea1c857d
Axel <axel.adrian@me.com>
2021-01-27 16:19:01 +0100

    Remove code

--- packages/app/services/search.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const serverUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}/search`
-
-export function searchFunc(searchString) {
-  return axios.get(`${serverUrl}/${searchString}`)
-}
